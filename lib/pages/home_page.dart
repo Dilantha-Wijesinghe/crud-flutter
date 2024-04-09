6e9de478c4db1f0e59911d1b22a6cae51f838da2
Dilantha Wijesinghe <wdilantha66@gmail.com>
2024-04-10 01:24:35 +0530

    feat: Connect firebase

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,57 @@
+import 'package:crud_flutter/services/firestore.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+// firestore
+  final FirestoreService firestoreService = FirestoreService();
+
+  // text controller
+  final TextEditingController textController = TextEditingController();
+
+  // open a dialog box to add a new note
+  void openNoteBox() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        // text user input
+        content: TextField(
+          controller: textController,
+        ),
+        actions: [
+          // button to save
+          ElevatedButton(
+            onPressed: () {
+              // add a new note
+              firestoreService.addNote(textController.text);
+
+              // clear the text controller
+              textController.clear();
+
+              // close the dialog box
+              Navigator.pop(context);
+            },
+            child: const Text("Add"),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Notes')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: openNoteBox,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
