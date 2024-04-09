f44811b71b92cee44198c30ad98bbf81e5d56bc8
Dilantha Wijesinghe <wdilantha66@gmail.com>
2024-04-10 02:26:07 +0530

    feat: Update and delete operations

--- lib/services/firestore.dart
+++ lib/services/firestore.dart
@@ -14,8 +14,23 @@ class FirestoreService {
   }
 
   // READ: get notes from the database
+  Stream<QuerySnapshot> getNotesStream() {
+    final notesStream =
+        notes.orderBy("timestamp", descending: true).snapshots();
+
+    return notesStream;
+  }
 
   // UPDATE: update notes given a doc id
+  Future<void> updateNote(String docID, String newNote) {
+    return notes.doc(docID).update({
+      'note': newNote,
+      'timestamp': Timestamp.now(),
+    });
+  }
 
   // DELETE: delete notes given a doc id
+  Future<void> deleteNote(String docID) {
+    return notes.doc(docID).delete();
+  }
 }
